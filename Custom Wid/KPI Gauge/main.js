bfa7566e7358b8acb64061d7214b8bc38eab5d30
Abishek718 <88944058+Abishek718@users.noreply.github.com>
2024-01-05 12:34:18 +0530

    Update main.js

--- Custom Wid/KPI Gauge/main.js
+++ Custom Wid/KPI Gauge/main.js
@@ -12,7 +12,7 @@ var getScriptPromisify = (src) => {
       <div id="root" style="width: 100%; height: 100%;">
       </div>
     `
-  class SamplePrepared extends HTMLElement {
+  class SampleKPI extends HTMLElement {
     constructor () {
       super()
 
@@ -64,5 +64,5 @@ var getScriptPromisify = (src) => {
     }
   }
 
-  customElements.define('com-sap-sample-echarts-prepared', SamplePrepared)
+  customElements.define('com-sap-sample-echarts-prepared', SampleKPI)
 })()
