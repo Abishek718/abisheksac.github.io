2d3f5dc6996a9def6dff33f70418278f62e18797
Abishek718 <88944058+Abishek718@users.noreply.github.com>
2024-03-15 15:30:38 +0530

    Update Js script.js

--- Custom Wid/new/Js script.js
+++ Custom Wid/new/Js script.js
@@ -42,7 +42,7 @@ const url = "https://api.openai.com/v1";
     async post(apiKey, endpoint, prompt) {
       const { response } = await ajaxCall(
         apiKey,
-        `${url}/endpoint`,
+        `${url}/completions`,
         prompt
       );
       //console.log(response.choices[0].text);
