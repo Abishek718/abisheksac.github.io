7beef3c11fce86688fdb58907a4c74dc84075730
Abishek718 <88944058+Abishek718@users.noreply.github.com>
2024-04-11 18:00:27 +0530

    Update main.js

--- Custom Wid/New1/main.js
+++ Custom Wid/New1/main.js
@@ -1,75 +1,63 @@
 var getScriptPromisify = (src) => {
-  return new Promise(resolve => {
-    $.getScript(src, resolve)
-  })
-}
-
-(function () {
-  const prepared = document.createElement('template')
-  prepared.innerHTML = `
-      <style>
-      </style>
-      <div id="root" style="width: 100%; height: 100%;">
-      </div>
-    `
-  class SamplePrepared extends HTMLElement {
-    constructor () {
-      super()
-
-      this._shadowRoot = this.attachShadow({ mode: 'open' })
-      this._shadowRoot.appendChild(prepared.content.cloneNode(true))
-
-      this._root = this._shadowRoot.getElementById('root')
-
-      this._props = {}
-
-      this.render()
-    }
-
-    onCustomWidgetResize (width, height) {
-      this.render()
-    }
-
-    async render () {
-      await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
-
-      var dom = document.getElementById('chart-container');
-var myChart = echarts.init(dom, null, {
-  renderer: 'canvas',
-  useDirtyRect: false
-});
-var app = {};
-var ROOT_PATH = 'https://echarts.apache.org/examples';
-var option;
-
-option = {
-  backgroundColor: '#000',
-  globe: {
-    baseTexture: ROOT_PATH + '/data-gl/asset/earth.jpg',
-    shading: 'lambert',
-    environment: ROOT_PATH + '/data-gl/asset/starfield.jpg',
-    atmosphere: {
-      show: true
-    },
-    light: {
-      ambient: {
-        intensity: 1.5
-      },
-      main: {
-        intensity: 1.5
+    return new Promise(resolve => {
+      $.getScript(src, resolve)
+    })
+  }
+  
+  (function () {
+    const prepared = document.createElement('template')
+    prepared.innerHTML = `
+        <style>
+        </style>
+        <div id="root" style="width: 100%; height: 100%;">
+        </div>
+      `
+    class SamplePrepared extends HTMLElement {
+      constructor () {
+        super()
+  
+        this._shadowRoot = this.attachShadow({ mode: 'open' })
+        this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+  
+        this._root = this._shadowRoot.getElementById('root')
+  
+        this._props = {}
+  
+        this.render()
+      }
+  
+      onCustomWidgetResize (width, height) {
+        this.render()
+      }
+  
+      async render () {
+        await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
+  
+        const chart = echarts.init(this._root)
+        const option = {
+            backgroundColor: '#000',
+            globe: {
+              baseTexture: 'https://echarts.apache.org/examples/data-gl/asset/earth.jpg',
+              shading: 'lambert',
+              environment: 'https://echarts.apache.org/examples/data-gl/asset/starfield.jpg',
+              atmosphere: {
+                show: true
+              },
+              light: {
+                ambient: {
+                  intensity: 0.1
+                },
+                main: {
+                  intensity: 1.5
+                }
+              }
+            },
+            series: []
+          }
+        chart.setOption(option)
       }
     }
-  },
-  series: []
-};
-
-if (option && typeof option === 'object') {
-  myChart.setOption(option);
-}
-
-window.addEventListener('resize', myChart.resize);
-    }
-  }
-
-  customElements.define('com-sap-sample-echarts-prepared', SamplePrepared)
-})()
+  
+    customElements.define('com-sap-sample-echarts-prepared', SamplePrepared)
+  })()
+  
