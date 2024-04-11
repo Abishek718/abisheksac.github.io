659408794953dcdb56a04f549382cd033efc2cd6
Abishek718 <88944058+Abishek718@users.noreply.github.com>
2024-04-11 17:43:47 +0530

    Update main.js

--- Custom Wid/New1/main.js
+++ Custom Wid/New1/main.js
@@ -1,63 +1,32 @@
-var getScriptPromisify = (src) => {
-  return new Promise(resolve => {
-    $.getScript(src, resolve)
-  })
-}
-
-(function () {
-  const prepared = document.createElement('template')
-  prepared.innerHTML = `
-      <style>
-      </style>
-      <div id="root" style="width: 100%; height: 100%;">
-      </div>
-    `
-  class SamplePrepared extends HTMLElement {
-    constructor () {
-      super()
-
-      this._shadowRoot = this.attachShadow({ mode: 'open' })
-      this._shadowRoot.appendChild(prepared.content.cloneNode(true))
-
-      this._root = this._shadowRoot.getElementById('root')
-
-      this._props = {}
-
-      this.render()
+var echarts = require('echarts');
+require('echarts-gl');
+ 
+var ROOT_PATH = 'https://echarts.apache.org/examples';
+ 
+var chartDom = document.getElementById('main');
+var myChart = echarts.init(chartDom);
+var option;
+ 
+option = {
+  backgroundColor: '#000',
+  globe: {
+    baseTexture: ROOT_PATH + '/data-gl/asset/starfield.jpg',
+    shading: 'lambert',
+    //environment: ROOT_PATH + '/data-gl/asset/starfield.jpg',
+    atmosphere: {
+      show: true
+    },
+    light: {
+      ambient: {
+        intensity: 1.5
+      },
+      main: {
+        intensity: 0.5
+      }
     }
-
-    onCustomWidgetResize (width, height) {
-      this.render()
-    }
-
-    async render () {
-      await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
-
-      const chart = echarts.init(this._root)
-      var ROOT_PATH = 'https://echarts.apache.org/examples';
-      const option = {
-        backgroundColor: '#000',
-        globe: {
-          baseTexture: ROOT_PATH + '/data-gl/asset/earth.jpg',
-          shading: 'lambert',
-          environment: ROOT_PATH + '/data-gl/asset/starfield.jpg',
-          atmosphere: {
-            show: true
-          },
-          light: {
-            ambient: {
-              intensity: 0.1
-            },
-            main: {
-              intensity: 1.5
-            }
-          }
-        },
-        series: []
-      };
-      chart.setOption(option)
-    }
-  }
-
-  customElements.define('com-sap-sample-echarts-prepared', SamplePrepared)
-})()
+  },
+  series: []
+};
+ 
+option && myChart.setOption(option);
+customElements.define('com-sap-sample-echarts-prepared', SamplePrepared)
