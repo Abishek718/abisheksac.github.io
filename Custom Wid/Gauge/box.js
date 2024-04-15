fa309ccf58ebee7b8d6244a3aba30d2b38bdffbe
Abishek718 <88944058+Abishek718@users.noreply.github.com>
2024-04-15 13:04:41 +0530

    Update box.js

--- Custom Wid/Gauge/box.js
+++ Custom Wid/Gauge/box.js
@@ -76,7 +76,7 @@
 		render(val, info, color) {
 			var val1 = val * 0.01;
 			var x = this.svg_circle_arc_path(500, 500, 450, -90, val1 * 180.0 - 90);
-			var rounded = Math.round( val * 10 ) / 10;
+			var rounded = Math.round( val * 100 ) / 100;
 
 			
 			if(rounded >=0 && rounded <=200) {
